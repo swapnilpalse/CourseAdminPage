7e8af4d4e5b0acbd62e073dd3d34721a38a508a2
Palse,Swapnil <swapnil.palse@gmail.com>
2018-09-12 14:21:29 -0500

    Adding the react files which contains components and the routes for the application's initial stage

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {PropTypes} from 'react';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="container-fluid">
+        <p> Header here ... </p>
+        {
+          this.props.children
+        }
+      </div>
+    );
+  }
+}
+
+App.propTypes={
+  childre:PropTypes.object.isRequired
+};
+
+export default App;
--- tools/startMEssage.js
+++ tools/startMEssage.js
@@ -1,3 +1,3 @@
 import colors from 'colors';
 
-console.log('Starting app in Dev mode...'.green);
+//console.log('Starting app in Dev mode...'.green);
